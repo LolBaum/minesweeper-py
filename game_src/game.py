5eb80d77150db6f7efd6881fd01c262775bdf258
Baum-TestTester <laurin.dahm@holoplot.com>
2023-08-09 16:21:17 +0200

    discover empty fields on click

--- game_src/game.py
+++ game_src/game.py
@@ -11,14 +11,17 @@ def basic_surf(size, color):
     surf.fill(color)
     return surf
 
+g_size = 20
 
-MINE = basic_surf(30, (255, 0, 0))
-NUMBER = basic_surf(30, (100, 100, 200))
-EMPTY_FIELD = basic_surf(30, (180, 180, 180))
-HIDDEN = basic_surf(30, (144, 144, 144))
 
-NEIGBOUR_INDICES = [x for x in product((-1, 0, 1), (-1, 0, 1))]
-NEIGBOUR_INDICES.remove((0, 0))
+MINE = basic_surf(g_size, (255, 0, 0))
+NUMBER = basic_surf(g_size, (100, 100, 200))
+EMPTY_FIELD = basic_surf(g_size, (180, 180, 180))
+HIDDEN = basic_surf(g_size, (144, 144, 144))
+
+NEIGBOUR_INDICES_AXIS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+NEIGBOUR_INDICES_DIAGONAL = [(-1, -1), (1, 1), (1, -1), (-1, 1)]
+NEIGBOUR_INDICES = NEIGBOUR_INDICES_AXIS + NEIGBOUR_INDICES_DIAGONAL
 
 # Todo: free connected empy fields when clicked
 # todo: display Neigbour Mines number
@@ -32,20 +35,25 @@ NEIGBOUR_INDICES.remove((0, 0))
 # todo: display GAME OVER
 
 
-
 class Field:
     def __init__(self, rect: pygame.rect.Rect, value: int = 0):
         self.value = value
         self.hidden = True
         self.is_mine = False
         self.rect = rect
+        self.changed = True
 
-    def on_click(self, display: pygame.surface.Surface):
-        self.hidden = False
-        self.draw(display)
-        print(f"clicked {self.value}")
+    def _set_hidden(self, hidden):
+        self.hidden = hidden
+        self.changed = True
+
+    def on_click(self):
+        self._set_hidden(False)
+        print(f"freed {self.value}")
 
     def draw(self, display: pygame.surface.Surface):
+        if not self.changed:
+            return
         pos = self.rect.topleft
         if self.hidden:
             display.blit(HIDDEN, pos)
@@ -58,11 +66,11 @@ class Field:
 
 
 class Board:
-    def __init__(self, shape: [int, int] = (15, 15), num_mines: int = 35, pos=(0,0)):
+    def __init__(self, shape: [int, int] = (15, 15), num_mines: int = 35, pos=(0, 0), field_size=30):
         self.num_mines = num_mines
         self.shape = shape
         self.padding = 3
-        self.filed_size = 30
+        self.filed_size = field_size
         self.image_size = (self.shape[0] * self.filed_size + (self.shape[0] - 1) * self.padding,
                            self.shape[1] * self.filed_size + (self.shape[1] - 1) * self.padding)
         self.rect = pygame.rect.Rect(pos, self.image_size)
@@ -70,13 +78,19 @@ class Board:
         self.b = self.setup()
         self.place_mines()
         self.count_mine_neighbours()
-        self.has_changed = True
+        self.changed = True
 
     def is_mine(self, x: int, y: int) -> bool:
         if x < 0 or x >= self.shape[0] or y < 0 or y >= self.shape[1]:
             return False
         else:
             return self.b[x][y].is_mine
+
+    def has_neighbour_mines(self, x: int, y: int) -> bool:
+        if x < 0 or x >= self.shape[0] or y < 0 or y >= self.shape[1]:
+            return False
+        else:
+            return self.b[x][y].value > 0
 
     def setup(self):
         b = []
@@ -108,9 +122,11 @@ class Board:
             self.b[x][y].is_mine = True
 
     def draw(self, display):
-        for row in self.b:
-            for f in row:
-                f.draw(display)
+        if self.changed:
+            for row in self.b:
+                for f in row:
+                    f.draw(display)
+            self.changed = False
 
     def find_index(self, x, y) -> list[int, int] | None:
         for i in range(self.shape[0]):
@@ -118,11 +134,31 @@ class Board:
                 if self.b[i][j].rect.collidepoint(x, y):
                     return i, j
 
-    def click_field(self, x, y, display: pygame.surface.Surface):
+    def click_field(self, x, y):
         idx = self.find_index(x, y)
         if idx:
             i, j = idx
-            self.b[i][j].on_click(display)
+            self.b[i][j].on_click()
+            self.discover(i, j)
+            self.changed = True
+
+    def discover(self, i, j, disc=True):
+        if self.has_neighbour_mines(i, j):
+            self.b[i][j].on_click()
+        else:
+            if disc:
+                self.b[i][j].on_click()
+                for n, m in NEIGBOUR_INDICES_AXIS:
+                    x = n+i
+                    y = m+j
+                    if self.shape[0] > x >= 0 and self.shape[1] > y >= 0 and self.b[x][y].hidden:
+                        self.discover(x, y, disc=True)
+                for n, m in NEIGBOUR_INDICES_DIAGONAL:
+                    x = n+i
+                    y = m+j
+                    if self.shape[0] > x >= 0 and self.shape[1] > y >= 0 and self.b[x][y].hidden:
+                        self.discover(x, y, disc=False)
+
 
 
 
@@ -130,12 +166,12 @@ class Board:
 if __name__ == "__main__":
     print("running python Minesweeper")
     pygame.init()
-    w, h = 700, 940
+    w, h = 940, 940
     screen = pygame.display.set_mode((w, h))
     clock = pygame.time.Clock()
     selection_active = False
 
-    board = Board()
+    board = Board(shape=(30, 30), field_size=g_size)
     print("drawing Board")
     board_pos = ((w - board.image_size[0])/2, 150)
     board.draw(screen)
@@ -144,6 +180,7 @@ if __name__ == "__main__":
     while True:
         pygame.display.flip()
         clock.tick(30)
+        board.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print('Shutting down the game')
@@ -158,7 +195,7 @@ if __name__ == "__main__":
                     x, y = pygame.mouse.get_pos()
                     if board.rect.collidepoint(x, y):
                         print("clicked Board")
-                        board.click_field(x, y, screen)
+                        board.click_field(x, y)
 
             # TODO: mouse botten down
             #if pygame.mouse.get_pressed(num_buttons=3)[0]:  # left mouse button
